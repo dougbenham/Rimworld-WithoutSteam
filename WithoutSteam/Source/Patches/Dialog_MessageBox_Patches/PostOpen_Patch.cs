d60afefa7cb576b3d728979473a2626ba6ff2296
Doug Benham <douglasabenham@gmail.com>
2024-04-16 00:05:52 -0700

    Updated to 1.5

--- WithoutSteam/Source/Patches/Dialog_MessageBox_Patches/PostOpen_Patch.cs
+++ WithoutSteam/Source/Patches/Dialog_MessageBox_Patches/PostOpen_Patch.cs
@@ -8,11 +8,10 @@ namespace WithoutSteam.Patches.Dialog_MessageBox_Patches {
     public static class PostOpen_Patch {
 
         public static void Postfix(this Window __instance) {
-            if (__instance != null && __instance is Dialog_MessageBox) {
-                var dialogBox = __instance as Dialog_MessageBox;
-                string steamText = "SteamClientMissing".Translate();
+            if (__instance is Dialog_MessageBox dialogBox) {
+	            string steamText = "SteamClientMissing".Translate();
                 if (dialogBox.text == steamText) {
-                    __instance.Close();
+                    dialogBox.Close();
                     Log.Message("WithoutSteam closed a 'SteamClientMissing' window");
                 }
             }
