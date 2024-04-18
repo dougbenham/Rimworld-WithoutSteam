09df6aaee77f5fc531b76d9e2d991b9918b2c82b
Doug Benham <douglasabenham@gmail.com>
2024-04-18 00:27:05 -0700

    Reorganized

---
+++ Source/Main.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace WithoutSteam
+{
+	[StaticConstructorOnStartup]
+	public class Main : Mod
+	{
+		public Main(ModContentPack content) : base(content)
+		{
+			var harmony = new Harmony("doug.withoutsteam");
+			harmony.PatchAll();
+		}
+	}
+}
---
+++ Source/Patches/Dialog_MessageBox_Patches/PostOpen_Patch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace WithoutSteam.Patches.Dialog_MessageBox_Patches
+{
+	[HarmonyPatch]
+	[HarmonyPatch(typeof(Window), "PostOpen")]
+	public static class PostOpen_Patch
+	{
+		public static void Postfix(this Window __instance)
+		{
+			if (__instance is Dialog_MessageBox dialogBox)
+			{
+				string steamText = "SteamClientMissing".Translate();
+				if (dialogBox.text == steamText)
+				{
+					dialogBox.Close();
+					Log.Message("WithoutSteam closed a 'SteamClientMissing' window");
+				}
+			}
+		}
+	}
+}
